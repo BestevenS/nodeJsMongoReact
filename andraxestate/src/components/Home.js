8066c9d8560f09bb7a428e2ca52771cf381fd5d5
Besteven <stefanostei18@gmail.com>
2023-07-06 01:46:22 +0300

    Home.js ready.

--- andraxestate/src/components/Home.js
+++ andraxestate/src/components/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-function Home() {
-  return (
-    <div>
-      <h1>Καλώς ήρθατε στο μεσιτικό γραφείο μας!</h1>
-      // εδώ μπορείτε να προσθέσετε περισσότερο HTML ή άλλα components
-    </div>
-  );
+const Home = () => {
+    return (
+        <div>
+            <h1>Αρχική Σελίδα</h1>
+        </div>
+    )
 }
 
 export default Home;
