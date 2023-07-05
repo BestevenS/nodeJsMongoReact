b232e9c4de77b3357dc471f5618bccd3b6e1b6a8
Besteven <stefanostei18@gmail.com>
2023-07-05 21:17:30 +0300

    add update and delete routes to the users and properties

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const propertyController = require('../controllers/propertyController');
 
 // Όλα τα ακίνητα
-router.get('/', propertyController.getAllProperties);
+router.get('/', propertyController.getProperties);
 
 // Εισαγωγή νέου ακινήτου
 router.post('/', propertyController.createProperty);
 
 // Εμφάνιση συγκεκριμένου ακινήτου βάση ID
-router.get('/:id', propertyController.getPropertyById);
+router.get('/:id', propertyController.getProperty);
 
 // Ενημέρωση συγκεκριμένου ακινήτου βάση ID
-router.put('/:id', propertyController.updatePropertyById);
+router.put('/:id', propertyController.updateProperty);
 
 // Διαγραφή συγκεκριμένου ακινήτου βάση ID
-router.delete('/:id', propertyController.deletePropertyById);
+router.delete('/:id', propertyController.deleteProperty);
 
 module.exports = router;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-
 const auth = require('../middlewares/auth');
 
 // Εγγραφή χρήστη
-router.post('/register', userController.registerUser);
+router.post('/register', userController.createUser);
 
 // Σύνδεση χρήστη
 router.post('/login', userController.loginUser);
 
 // Προφίλ χρήστη
-router.get('/profile', auth, userController.getUserProfile);
+router.get('/profile', auth, userController.getUser);
 
 // Ενημέρωση προφίλ χρήστη
-router.put('/profile', auth, userController.updateUserProfile);
+router.put('/profile', auth, userController.updateUser);
+
+// Λίστα όλων των χρηστών
+router.get('/', userController.getUsers);
+
+// Διαγραφή χρήστη
+router.delete('/:id', auth, userController.deleteUser);
 
 module.exports = router;
