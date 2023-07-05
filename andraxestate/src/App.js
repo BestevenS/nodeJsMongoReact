1a7470a5cf8986f6ba1a1bc962bf83ff642c1d8f
Besteven <stefanostei18@gmail.com>
2023-07-06 01:11:43 +0300

    Add SearchPage to app.js for the routing

--- andraxestate/src/App.js
+++ andraxestate/src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import SearchPage from './components/SearchPage';
-import PropertyPage from './components/PropertyPage';
-import UserPage from './components/UserPage';
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./Home";
+import SearchPage from "./SearchPage";
+// Προσθέστε άλλες εισαγωγές εδώ όταν δημιουργήσετε τις σελίδες
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/search" component={SearchPage} />
-        <Route path="/property/:id" component={PropertyPage} />
-        <Route path="/user" component={UserPage} />
-        {/* μπορείτε να προσθέσετε περισσότερα routes εδώ */}
+        {/* Προσθέστε άλλα routes εδώ όταν δημιουργήσετε τις σελίδες */}
       </Switch>
     </Router>
   );
