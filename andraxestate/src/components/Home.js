5a3597d0f2b5282de3a7dd33a9e33d4e64144e70
Besteven <stefanostei18@gmail.com>
2023-07-06 01:02:53 +0300

    add home.js

---
+++ andraxestate/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Καλώς ήρθατε στο μεσιτικό γραφείο μας!</h1>
+      // εδώ μπορείτε να προσθέσετε περισσότερο HTML ή άλλα components
+    </div>
+  );
+}
+
+export default Home;
