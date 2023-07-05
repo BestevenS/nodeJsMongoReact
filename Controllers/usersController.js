331799c80eb2c3d7a3ce86fba8d7a4efc3af4b35
Besteven <stefanostei18@gmail.com>
2023-07-05 15:00:22 +0300

    add usersController.js file in controllers ;

---
+++ Controllers/usersController.js
@@ -0,0 +1,113 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+// Create
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ name, email, password: hashedPassword });
+    const user = await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      data: user
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error
+    });
+  }
+};
+
+// Read (Get all users)
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      count: users.length,
+      data: users
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error
+    });
+  }
+};
+
+// Read (Get single user)
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'No user found'
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: user
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error
+    });
+  }
+};
+
+// Update
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'No user found'
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: user
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error
+    });
+  }
+};
+
+// Delete
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: 'No user found'
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error
+    });
+  }
+};
