a1c12e22bb3a8dd3bec691620b844b1e11230ad2
Besteven <stefanostei18@gmail.com>
2023-07-06 01:09:55 +0300

    Add two more files to src

--- andraxestate/src/App.js
+++ andraxestate/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import SearchPage from './components/SearchPage';
+import PropertyPage from './components/PropertyPage';
+import UserPage from './components/UserPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/search" component={SearchPage} />
+        <Route path="/property/:id" component={PropertyPage} />
+        <Route path="/user" component={UserPage} />
+        {/* μπορείτε να προσθέσετε περισσότερα routes εδώ */}
+      </Switch>
+    </Router>
   );
 }
 
