da0c6fe27857264525699eb957a6ceaaf49de28c
Besteven <stefanostei18@gmail.com>
2023-07-09 17:18:48 +0300

    fix a router error in app.js

--- andraxestate/src/App.js
+++ andraxestate/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import SearchPage from "./components/SearchPage";
 import PropertyPage from "./components/PropertyPage";
@@ -8,14 +8,12 @@ import UserPage from "./components/UserPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/property" element={<PropertyPage />} />
-        <Route path="/user" element={<UserPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/property" element={<PropertyPage />} />
+      <Route path="/user" element={<UserPage />} />
+    </Routes>
   );
 }
 
