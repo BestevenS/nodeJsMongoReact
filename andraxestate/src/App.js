698e6e21e97da6b45a77ff69598e2aa08d9fbb18
Besteven <stefanostei18@gmail.com>
2023-07-09 20:53:08 +0300

    Recreate the folder andraxestate

--- andraxestate/src/App.js
+++ andraxestate/src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import SearchPage from "./components/SearchPage";
-import PropertyPage from "./components/PropertyPage";
-import UserPage from "./components/UserPage";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/property" element={<PropertyPage />} />
-      <Route path="/user" element={<UserPage />} />
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
