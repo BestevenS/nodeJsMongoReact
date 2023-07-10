9185744364ae27f5823b1bfe7cbfc513f36af495
Besteven <stefanostei18@gmail.com>
2023-07-11 00:06:55 +0300

    not working the img but i will fix it

--- andraxestate/src/components/HomePage.js
+++ andraxestate/src/components/HomePage.js
@@ -3,7 +3,9 @@ import React from 'react';
 function HomePage() {
   return (
     <div>
-      <h1>Καλώς ήρθατε στο μεσιτικό γραφείο μας!</h1>
+          <h1>Καλωσόρισες στο AndraxEstate!</h1>
+          <p>Εδώ θα βρείτε τα καλύτερα ακίνητα για ενοικίαση και πώληση.</p>
+          <img src="../../assets/png1.png" alt="Real estate banner" />
     </div>
   );
 }
