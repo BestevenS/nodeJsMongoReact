2762a22fcc124f8cec61d6e23e5305e1c39205bd
Besteven <stefanostei18@gmail.com>
2023-07-10 22:31:05 +0300

    fix all errors and this a good checkpoint to start coding

--- andraxestate/src/AppRouter.js
+++ andraxestate/src/AppRouter.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
 import SearchPage from './components/Search/SearchPage';
-import PropertyDetailsPage from './components/Property/PropertyPage';
+import PropertyDetailsPage from './components/Property/PropertyDetailsPage';
 import UserPage from './components/User/UserPage';
 
-function App() {
+function AppRouter() {
   return (
     <Router>
-      <Routes>
+      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/search" component={SearchPage} />
         <Route path="/property/:id" component={PropertyDetailsPage} />
         <Route path="/user/:id" component={UserPage} />
-      </Routes>
+      </Switch>
     </Router>
   );
 }
 
-export default App;
+export default AppRouter;
