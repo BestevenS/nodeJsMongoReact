af881f6d21e46f30bc7ed3c2d441646716e60c4b
Besteven <stefanostei18@gmail.com>
2023-07-18 15:08:32 +0300

    just go to the basic version of react

--- andraxestate/src/App.js
+++ andraxestate/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import HomePage from './components/HomePage';
-import SearchPage from './components/Search/SearchPage';
-import PropertyDetailsPage from './components/Property/PropertyPage';
-import UserPage from './components/User/UserPage';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/property/:id" element={<PropertyDetailsPage />} />
-        <Route path="/user/:id" element={<UserPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
